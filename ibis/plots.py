175627216114070c063b6ee60ed9d68befa176d9
Tomek <owerko@gmail.com>
2020-08-24 10:37:33 +0200

    Linear regression

--- ibis/plots.py
+++ ibis/plots.py
@@ -88,13 +88,13 @@ print(indeksy)
 
 dealta39 = []
 for i in range(len(indeksy)):
-    d= tc39[i] - model.predict(X)[indeksy[i]]
+    d = tc39[i] - model.predict(X)[indeksy[i]]
     dealta39.append(d)
 print(dealta39)
 
 dealta23 = []
 for i in range(len(indeksy)):
-    d= tc23[i] - model23.predict(X)[indeksy[i]]
+    d = tc23[i] - model23.predict(X)[indeksy[i]]
     dealta23.append(d)
 print(dealta23)
 
@@ -102,32 +102,62 @@ print(mean(dealta23), stdev(dealta23))
 print(mean(dealta39), stdev(dealta39))
 
 # plt.figure(1)
-fig, ax = plt.subplots(figsize=(8, 4), dpi=300)
-plt.subplots_adjust(bottom=0.3)
-plt.plot(date, Rbin39, 'k', label='IBIS data')
-plt.plot(date, Y_pred, 'm-.', label='IBIS linear regression')
-plt.plot(datetc, tc39, 'ro', label='TC')
-# plt.plot(datetc, Y_pred_tc39, 'g-', label='TC linear regression')
-# plt.plot(date, Rbin39)
-plt.xticks(rotation=90)
-plt.xlabel('Time [hh:mm:ss]')
-plt.ylabel('Deformation [mm]')
-plt.title('Rbin 39 observed with IBIS and TC')
-ax.xaxis.set_major_locator(ticker.LinearLocator(10))
-plt.legend()
-plt.show()
+# fig, ax = plt.subplots(figsize=(8, 4), dpi=300)
+# plt.subplots_adjust(bottom=0.3)
+# plt.plot(date, Rbin39, 'k', label='IBIS data')
+# plt.plot(date, Y_pred, 'm-.', label='IBIS linear regression')
+# plt.plot(datetc, tc39, 'ro', label='TC')
+# # plt.plot(datetc, Y_pred_tc39, 'g-', label='TC linear regression')
+# # plt.plot(date, Rbin39)
+# plt.xticks(rotation=90)
+# plt.xlabel('Time [hh:mm:ss]')
+# plt.ylabel('Deformation [mm]')
+# plt.title('Rbin 39 observed with IBIS and TC')
+# ax.xaxis.set_major_locator(ticker.LinearLocator(10))
+# plt.legend()
+# plt.show()
+#
+# fig, ax = plt.subplots(figsize=(8, 4), dpi=300)
+# plt.subplots_adjust(bottom=0.34)
+# plt.plot(date, Rbin23, 'k', label='IBIS data')
+# plt.plot(date, Y_pred23, 'm-.', label='IBIS linear regression')
+# plt.plot(datetc, tc23, 'ro', label='TC')
+# # plt.plot(datetc, Y_pred_tc23, 'g-', label='TC linear regression')
+# # plt.scatter(datetc, tc23, marker='o', color='g', edgecolors='black', alpha=0.5, label="TC")
+# plt.xticks(rotation=90)
+# plt.xlabel('Time [hh:mm:ss]')
+# plt.ylabel('Deformation [mm]')
+# plt.title('Rbin 23 observed with IBIS and TC')
+# ax.xaxis.set_major_locator(ticker.LinearLocator(10))
+# plt.legend()
+# plt.show()
+
+xx = [1, 2, 3, 4, 5, 6, 7]
+print(dealta23)
+print(dealta39)
+print(xx)
+
+dx = np.asarray(xx).reshape(-1,1)
+
+modeldiff23 = LinearRegression().fit(dx, dealta23)
+modeldiff39 = LinearRegression().fit(dx, dealta39)
+
+y_pred_d23 = modeldiff23.predict(dx)
+y_pred_d39 = modeldiff39.predict(dx)
+
+r2 = modeldiff23.score(dx, dealta23)
+print(f'Intercept: {modeldiff23.intercept_}, Slope: {modeldiff23.coef_}, E2: {r2}')
+
+r2 = modeldiff23.score(dx, dealta39)
+print(f'Intercept: {modeldiff39.intercept_}, Slope: {modeldiff39.coef_}, E2: {r2}')
+
 
 fig, ax = plt.subplots(figsize=(8, 4), dpi=300)
-plt.subplots_adjust(bottom=0.34)
-plt.plot(date, Rbin23, 'k', label='IBIS data')
-plt.plot(date, Y_pred23, 'm-.', label='IBIS linear regression')
-plt.plot(datetc, tc23, 'ro', label='TC')
-# plt.plot(datetc, Y_pred_tc23, 'g-', label='TC linear regression')
-# plt.scatter(datetc, tc23, marker='o', color='g', edgecolors='black', alpha=0.5, label="TC")
-plt.xticks(rotation=90)
-plt.xlabel('Time [hh:mm:ss]')
-plt.ylabel('Deformation [mm]')
-plt.title('Rbin 23 observed with IBIS and TC')
-ax.xaxis.set_major_locator(ticker.LinearLocator(10))
+plt.plot(xx, dealta23, '-ok', color='black', label='Rbin23 diff.')
+plt.plot(xx, y_pred_d23, 'k:', label='Rbin23 diff. LR')
+plt.plot(xx, dealta39, '-or', color='red', label='Rbin39 diff.')
+plt.plot(xx, y_pred_d39, 'r:', label='Rbin39 diff. LR')
+plt.xlabel('Observation no.')
+plt.ylabel('Difference TC - IBIS_LR[mm]')
 plt.legend()
 plt.show()
